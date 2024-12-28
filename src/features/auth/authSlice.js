7005d1d9aa6a44bc0357e7a86ffb8b4ebf228eda
Dawn Kim <kymkhw279@gmail.com>
2024-12-28 20:38:14 +0900

    auth fix 1

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,24 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
-import Cookies from "js-cookie";
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: { token: null },
+  initialState: { token: null, persist: true },
   reducers: {
     setCredentials: (state, action) => {
       const { accessToken } = action.payload;
       state.token = accessToken;
-      Cookies.set("token", accessToken); // Set cookie to expire in 30 seconds (1/48 of an hour)
     },
     logOut: (state) => {
       state.token = null;
-      Cookies.remove("token");
+    },
+    setPersist: (state, action) => {
+      state.persist = action.payload;
     },
   },
 });
 
-export const { setCredentials, logOut } = authSlice.actions;
+export const { setCredentials, logOut, setPersist } = authSlice.actions;
 
 export default authSlice.reducer;
 
 export const selectCurrentToken = (state) => state.auth.token;
+export const selectPersist = (state) => state.auth.persist;
